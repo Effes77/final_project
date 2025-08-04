ef78456b4e01f60b6f7295377fd42300bf5e3485
Effes77 <alexandervl928@gmail.com>
2025-08-04 18:45:30 +0300

    added_final_project

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+// js
+import '../js/click.js';
+import '../js/tabs.js';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+
+// css
+import 'swiper/css';
+import 'swiper/css/pagination';
+import '../scss/style.scss';
+
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+    modules: [Pagination],  
+    pagination: {
+        el: '.swiper-pagination',
+  },
+});
+
---
+++ src/js/tabs.js
@@ -0,0 +1,16 @@
+(function () {
+        const tabs = document.querySelectorAll (".tabs__link");
+    
+    const contents = document.querySelectorAll(".tabs__content-text");
+    
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+            tabs.forEach((n) => n.classList.remove("active"));
+            contents.forEach((m) => m.classList.remove("active"));
+
+            tab.classList.add("active");
+
+            contents[index].classList.add("active");
+        });   
+    });
+      })();
